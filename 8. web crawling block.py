3e58dbeef87c620b00ea93e2d15219c666dad163
godtechwak <68718384+godtechwak@users.noreply.github.com>
2020-08-09 20:35:11 +0900

    HTTP Header User Agent

---
+++ 8. web crawling block.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+import requests
+
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'}
+html = requests.get('https://search.zdnet.co.kr/', headers=headers).text
+print(html)
